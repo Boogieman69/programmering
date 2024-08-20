a0f5554592e0b0b45bdd1bac53b2f8f46968106a
Boogieman69 <Amirfly93@gmail.com>
2024-08-20 09:28:51 +0200

    github

--- 02-WEBTEMPLATE CSS tricks/index.js
+++ 02-WEBTEMPLATE CSS tricks/index.js
@@ -1,5 +1,6 @@
-let currentPage = 2 
+let currentPage = 3
 let pages //array med alle elementer med class = page
+let colors = ['red', 'green', 'blue', 'grey', 'pink', 'purple', 'black']
 
 function setup(){
     console.log('P5.js er loaded' )
@@ -7,6 +8,14 @@ function setup(){
     pages = selectAll('.page')
     //nu kan man se at alle pages er blevet til en liste med alle class = page ting 
     //console.log(pages.length)
+
+    //lav en masse div'er vi kommer ind i page 3
+    for(c of colors){
+        //console.log(c)
+        let div = createDiv()
+        div.style('background-color', c)
+        select('#page3').child(div)
+    }
 }
 
 function shiftPage(num){
